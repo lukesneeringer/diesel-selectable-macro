17b4e7c3a3b061860c8f87d16a95d36836e967b0
Luke Sneeringer <luke@sneeringer.com>
2022-09-23 16:37:01 -0700

    feat: Parse table name how Diesel 2 does.

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,17 @@
 //! that performs a Diesel query by key names, rather than position (as
 //! [`diesel::Queryable`] does).
 
+use std::ops::Deref;
+
 use darling::ast;
 use darling::FromDeriveInput;
 use darling::FromField;
-use darling::FromMeta;
 use proc_macro::TokenStream as TokenStream1;
 use proc_macro2::TokenStream;
 use quote::quote;
 use quote::ToTokens;
+use syn::parse::Parse;
+use syn::parse::ParseStream;
 use syn::parse_macro_input;
 use syn::Attribute;
 use syn::DeriveInput;
@@ -17,7 +20,7 @@ use syn::Generics;
 use syn::Ident;
 
 /// Provide a `.select()` function based on the struct's fields.
-#[proc_macro_derive(Selectable, attributes(table_name))]
+#[proc_macro_derive(Selectable, attributes(diesel))]
 #[cfg(not(tarpaulin_include))]
 pub fn derive_selectable(input: TokenStream1) -> TokenStream1 {
   SelectableStruct::from_derive_input(&parse_macro_input!(
@@ -31,7 +34,7 @@ pub fn derive_selectable(input: TokenStream1) -> TokenStream1 {
 /// Struct that receives the input struct to `Selectable` and augments it
 /// with the `.select()` function.
 #[derive(FromDeriveInput)]
-#[darling(supports(struct_named), forward_attrs(table_name))]
+#[darling(supports(struct_named), forward_attrs(diesel))]
 pub(crate) struct SelectableStruct {
   /// The name of the struct.
   ident: Ident,
@@ -68,19 +71,29 @@ impl ToTokens for SelectableStruct {
     // Put together our basic tokens: the struct identifier, and any generics
     // (types, lifetimes, etc.) we need to carry forward to the `Selectable`
     // implementation.
+
     let ident = &self.ident;
     let (impl_generics, type_generics, where_clause) =
       self.generics.split_for_impl();
 
     // Get the name of the table.
-    let meta = self
+    let diesel = self
       .attrs
       .iter()
-      .find(|attr| attr.path.is_ident("table"))
-      .expect("The `table` attribute is required")
-      .parse_meta()
-      .expect("Unable to parse `table` attribute");
-    let table_name: Ident = Ident::from_meta(&meta).expect("Bad identifier");
+      .find(|attr| attr.path.is_ident("diesel"))
+      .expect("The `diesel` attribute is required");
+    let args = syn::parse_macro_input::parse::<CommaSeparatedArguments>(
+      diesel.into_token_stream().into(),
+    )
+    .expect("Unable to parse arguments.");
+    let table_name = args
+      .iter()
+      .find_map(|arg| {
+        syn::parse_macro_input::parse::<TableNameParser>(arg.clone().into())
+          .ok()
+      })
+      .expect("No `table_name` argument found.")
+      .table_name;
     let table = quote! { crate::schema::#table_name::dsl::#table_name };
 
     // Get the list of fields as a tuple.
@@ -117,5 +130,58 @@ impl SelectableField {
   #[cfg(not(tarpaulin_include))]
   fn name(&self) -> &Ident {
     self.ident.as_ref().expect("Selectable only supports named fields")
+  }
+}
+
+struct CommaSeparatedArguments(Vec<TokenStream>);
+
+impl Parse for CommaSeparatedArguments {
+  fn parse(input: ParseStream) -> syn::Result<Self> {
+    let bracketed;
+    let content;
+    input.parse::<syn::Token![#]>()?;
+    syn::bracketed!(bracketed in input);
+    bracketed.parse::<syn::Ident>()?;
+    syn::parenthesized!(content in bracketed);
+
+    // There are zero or more arguments, comma separated. Split them up.
+    Ok(Self(
+      content
+        .parse_terminated::<TokenStream, syn::Token![,]>(TokenStream::parse)
+        .expect("Failed to parse comma-separated args")
+        .into_iter()
+        .collect(),
+    ))
+  }
+}
+
+impl Deref for CommaSeparatedArguments {
+  type Target = Vec<TokenStream>;
+
+  fn deref(&self) -> &Self::Target {
+    &self.0
+  }
+}
+
+struct TableNameParser {
+  table_name: Ident,
+}
+
+impl Parse for TableNameParser {
+  fn parse(input: ParseStream) -> syn::Result<Self> {
+    // We're looking for `table_name = foo`, so first we can split on `=` and
+    // collect the result; the length should be 2 if this is a match.
+    let key_val: Vec<Ident> = input
+      .parse_terminated::<Ident, syn::Token![=]>(Ident::parse)
+      .expect("Not = separated.")
+      .into_iter()
+      .collect();
+    if key_val.len() != 2 {
+      return Err(input.error("Incorrect token length."));
+    }
+    match key_val[0] == "table_name" {
+      true => Ok(Self { table_name: key_val[1].clone() }),
+      false => Err(input.error("Wrong attribute,")),
+    }
   }
 }
